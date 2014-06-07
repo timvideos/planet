f37d8cf556e76faa4badc7a9980eb56885c72477
Tim Videos Website Generator <generate@timvideos.us>
2014-06-07 13:33:04 +0000

    Converted https://github.com/timvideos/planet/commit/0c8410c1522d22b52af771c0b7783ed8fc025807

---
+++ js/planet.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+  fancyBoxInitializer.init();
+  ajaxPostLoader.init();
+});
+
+
+var fancyBoxInitializer = {
+
+  init: function(){
+    $('a').each(function () {
+      if ($(this).has('img')) {
+        $(this).fancybox();
+      }
+    });
+  }
+}
+
+var ajaxPostLoader = {
+
+  init: function(){
+    this.initShowMoreClick();
+    this.initHideMoreClick();
+  },
+
+  initShowMoreClick: function(){
+    $('#main').on('click', '.show-more', function(){
+      var $self = $(this),
+          $post = $self.closest('.post'),
+          $content = $post.find('.post-content'),
+          json_path = $post.data('json');
+
+      $self.addClass('disabled');
+      $self.text('Loading...');
+
+      if($content.text().length > 0){
+        $content.show();
+
+        ajaxPostLoader.showHideMoreButton($self);
+      }
+      else{
+        $.getJSON(json_path, function(data){
+          $content.html(data.content);
+
+          ajaxPostLoader.showHideMoreButton($self);
+        });
+      }
+
+    });
+  },
+
+  initHideMoreClick: function(){
+    $('#main').on('click', '.hide-more', function(){
+      var $self = $(this),
+          $post = $self.closest('.post'),
+          $content = $post.find('.post-content');      
+      
+      $self.addClass('disabled');
+      $self.text('Loading...');
+
+      $content.hide();
+
+      ajaxPostLoader.showShorMoreButton($self);
+    });
+  },
+
+  showHideMoreButton: function($self){
+    $self.removeClass('disabled');
+
+    $self.addClass('hide-more');
+    $self.removeClass('show-more');
+
+    $self.text('Hide post...');
+  },
+
+  showShorMoreButton: function($self){
+    $self.removeClass('disabled');
+
+    $self.addClass('show-more');
+    $self.removeClass('hide-more');
+
+    $self.text('Read more...');
+  }
+
+}
