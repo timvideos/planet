28ad5e3a348af6b75020156d9ef21c90663c765f
Tim Videos Website Generator <generate@timvideos.us>
2014-06-25 22:25:37 +0000

    Converted https://github.com/timvideos/planet/commit/a0cead1e4250efd90177d4092cf9d7eb14972c27

--- js/github-charts.js
+++ js/github-charts.js
@@ -9,6 +9,8 @@ var contributorsPieChartInitializer = {
       var projectData = ProjectsChartsData[project];
 
       contributorsPieChartInitializer.initializePieChartForProject(project, projectData.charts.pieChart);
+      contributorsPieChartInitializer.initializeCodeFrequencyForProject(project, projectData.charts.codeFrequency);
+      contributorsPieChartInitializer.initializeCommitActivityForProject(project, projectData.charts.commitActivity);
     }
   },
 
@@ -44,6 +46,102 @@ var contributorsPieChartInitializer = {
           name: 'Contributors',
           data: projectData
         }]
+      });
+    });
+  },
+
+  initializeCodeFrequencyForProject: function(project, projectData){
+    $(function(){
+      $('[data-project="'+ project +'"] .code-frequency').highcharts({
+        chart: {
+          type: 'area',
+          zoomType: 'x'
+        },
+        title: {
+          text: 'Code frequency statistics'
+        },
+        xAxis: {
+          type: 'datetime',
+          categories: projectData.series,
+          labels: {
+            formatter: function() {
+                return Highcharts.dateFormat('%b %Y', this.value);
+            }
+          },
+          tickInterval: 30 * 24 * 3600 * 1000,
+          // tickmarkPlacement: 'on',
+          title: {
+            enabled: false
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Lines of code'
+          },
+
+        },
+        tooltip: {
+          shared: true
+        },
+        plotOptions: {
+          area: {
+            stacking: 'normal',
+            lineColor: '#666666',
+            lineWidth: 1,
+            marker: {
+              enabled: false,
+            }
+          }
+        },
+        series: projectData.data
+      });
+    });
+  },
+
+
+  initializeCommitActivityForProject: function(project, projectData){
+    $(function(){
+      $('[data-project="'+ project +'"] .commit-activity').highcharts({
+        chart: {
+          zoomType: 'x'
+        },
+        title: {
+          text: 'Commits activity statistics'
+        },
+        xAxis: {
+          type: 'datetime',
+          categories: projectData.series,
+          labels: {
+            formatter: function() {
+                return Highcharts.dateFormat('%b %Y', this.value);
+            }
+          },
+          tickInterval: 30 * 24 * 3600 * 1000,
+          // tickmarkPlacement: 'on',
+          title: {
+            enabled: false
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Commits'
+          },
+
+        },
+        tooltip: {
+          shared: true
+        },
+        plotOptions: {
+          area: {
+            stacking: 'normal',
+            lineColor: '#666666',
+            lineWidth: 1,
+            marker: {
+              enabled: false,
+            }
+          }
+        },
+        series: projectData.data
       });
     });
   }
